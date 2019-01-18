cb1f51e9251452d596e9511f27c1132155349297
Junghoon Lee <iamfaddict@gmail.com>
2019-01-18 18:15:10 -0500

    adfadf

--- kern/synchprobs/traffic_synch.c
+++ kern/synchprobs/traffic_synch.c
@@ -221,7 +221,7 @@ intersection_after_exit(Direction origin, Direction destination)
   kprintf("south_cars: %d", south_cars);
   kprintf("east_cars: %d", east_cars);
   kprintf("west_cars: %d", west_cars);
-  exit_cars(origin, exited_cars);
+  exit_cars(origin, 1);
   if (exited_cars == 3 || waiting_cars(origin) >= 3) {
     remove_element(0);
     exited_cars = 0;
