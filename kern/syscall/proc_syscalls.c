febe736d5b2b557b942ebcf2eaf96d6cd40912d1
Junghoon Lee <iamfaddict@gmail.com>
2019-02-20 02:34:52 -0500

    adfaf

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -25,8 +25,8 @@ void sys__exit(int exitcode) {
      an unused variable */
   curproc->p_exit_code = exitcode;
   curproc->p_exited = true;
-  kprintf("&&&&&&&&&&&EXITING ON: %d\n", curproc->p_id);
-  kprintf("&&&&&&&EXITED: %d\n", curproc->p_exited);
+  // kprintf("&&&&&&&&&&&EXITING ON: %d\n", curproc->p_id);
+  // kprintf("&&&&&&&EXITED: %d\n", curproc->p_exited);
 
   V(curproc->p_sem);
 
@@ -117,8 +117,8 @@ sys_waitpid(pid_t pid,
   // ?
   if (!child->p_exited && child->p_id >= __PID_MIN && child->p_id <= __PID_MAX) {
     // kprintf("wait3.5\n");
-    kprintf("********WAITING: %d\n", child->p_exited);
-    kprintf("********WAITING ON: %d\n", child->p_id);
+    // kprintf("********WAITING: %d\n", child->p_exited);
+    // kprintf("********WAITING ON: %d\n", child->p_id);
 
     P(child->p_sem);
   }
@@ -173,7 +173,7 @@ int sys_fork(struct trapframe *tf, pid_t *retval) {
     proc_destroy(cp);
     return err;
   }
-  kprintf("********ASSIGNED ON: %d\n", cp->p_id);
+  // kprintf("********ASSIGNED ON: %d\n", cp->p_id);
 
   // kprintf("BP3\n");
 
