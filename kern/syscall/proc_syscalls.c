804804f7d6a06762219c5917d69f5b2259accbdb
Junghoon Lee <iamfaddict@gmail.com>
2019-02-20 03:20:12 -0500

    adfaf

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -48,6 +48,8 @@ void sys__exit(int exitcode) {
   V(curproc->p_sem);
   if (curproc->p_id == curproc->parent->in_wait_of) {
     P(curproc->parent->p_sem);
+  } else {
+    V(curproc->p_sem);
   }
   // if (curproc->parentP(curproc->parent->p_sem);
 
