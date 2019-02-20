662408125776204112c35ccd8ae1fd3c2b4269dc
Junghoon Lee <iamfaddict@gmail.com>
2019-02-20 02:02:42 -0500

    adfaf

--- kern/proc/proc.c
+++ kern/proc/proc.c
@@ -465,7 +465,7 @@ int proc_should_wait(pid_t tbf, struct proc *parent) {
 	kprintf("aloha");
 	for (unsigned i = 0; i < array_num(parent->children); i++) {
 		kprintf("shouldwait1 %d\n", ((struct proc *) array_get(parent->children, i))->p_id);
-		kprintf("shouldwait0 %d\n", ((struct proc *) array_get(parent->children, i))->p_exited);
+		kprintf("shouldwait0 %d\n", ((struct proc *) array_get(parent->children, i))->p_exited == true);
 		kprintf("shouldwait2 %d\n", tbf);
 		kprintf("shouldwait3 %d\n", ((struct proc *) array_get(parent->children, i))->p_id == tbf);
 		if (((struct proc *) array_get(parent->children, i))->p_id == tbf) return i;
