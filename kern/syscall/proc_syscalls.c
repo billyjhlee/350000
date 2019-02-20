b936b61a37bcc3308659debd5184b9821c6ebb0f
Junghoon Lee <iamfaddict@gmail.com>
2019-02-20 02:24:20 -0500

    adfaf

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -11,6 +11,7 @@
 #include <copyinout.h>
 #include <synch.h>
 #include <mips/trapframe.h>
+#include <kern/limits.h>
 
   /* this implementation of sys__exit does not do anything with the exit code */
   /* this needs to be fixed to get exit() and waitpid() working properly */
