9cd25ed58c546b27ce0b6d6dc3ff8ec0ac8be1dc
Junghoon Lee <iamfaddict@gmail.com>
2019-02-21 16:20:47 -0500

    v7-3

--- kern/proc/proc.c
+++ kern/proc/proc.c
@@ -229,7 +229,7 @@ proc_destroy(struct proc *proc)
 		kprintf("ll1\n");
 		pid_t *child_p_id = (pid_t *) array_get(proc->children, array_len - 1);
 		kprintf("ll2 %d\n", *child_p_id);
-		krpintf("ll0");
+		kprintf("ll0");
 		if (proc_states[*child_p_id - __PID_MIN]->p_exited){ 
 			kprintf("ll3\n");
 			remove_proc_state(*child_p_id);
