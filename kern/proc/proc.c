94685c1469d54c2d347f4c49726c79cc2a04a6d5
Junghoon Lee <iamfaddict@gmail.com>
2019-02-21 18:19:28 -0500

    fafff

--- kern/proc/proc.c
+++ kern/proc/proc.c
@@ -275,6 +275,14 @@ proc_destroy(struct proc *proc)
 void
 proc_bootstrap(void)
 {
+	p_id_manager = bitmap_create(__PID_MAX - __PID_MIN + 1);
+  	if (p_id_manager == NULL) {
+  		panic("could not create p_id_manager\n");
+  	}
+  	p_id_manager_lock = lock_create("p_id_manager_lock");
+  	if (p_id_manager_lock == NULL) {
+  		panic("could not create p_id_manager_lock\n");
+  	}
   kproc = proc_create("[kernel]");
   if (kproc == NULL) {
     panic("proc_create for kproc failed\n");
@@ -290,14 +298,6 @@ proc_bootstrap(void)
     panic("could not create no_proc_sem semaphore\n");
   }
 #endif // UW 
-  	p_id_manager = bitmap_create(__PID_MAX - __PID_MIN + 1);
-  	if (p_id_manager == NULL) {
-  		panic("could not create p_id_manager\n");
-  	}
-  	p_id_manager_lock = lock_create("p_id_manager_lock");
-  	if (p_id_manager_lock == NULL) {
-  		panic("could not create p_id_manager_lock\n");
-  	}
 }
 
 /*
--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -168,13 +168,13 @@ int sys_fork(struct trapframe *tf, pid_t *retval) {
   }
 
   // assign pid
-  err = proc_find_p_id(&cp->p_id);
-  // kprintf("ALLOC %d", cp->p_id);
+  // err = proc_find_p_id(&cp->p_id);
+  // // kprintf("ALLOC %d", cp->p_id);
 
-  if (err) {
-    proc_destroy(cp);
-    return err;
-  }
+  // if (err) {
+  //   proc_destroy(cp);
+  //   return err;
+  // }
 
   // GGG
   // kprintf("Assigned %d\n", cp->p_id);
