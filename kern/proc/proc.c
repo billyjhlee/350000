d56327f156edcf128f75f805375a588f2aaaf748
Junghoon Lee <iamfaddict@gmail.com>
2019-02-20 22:24:55 -0500

    v4 upgrade

--- kern/proc/proc.c
+++ kern/proc/proc.c
@@ -70,8 +70,8 @@ static struct semaphore *proc_count_mutex;
 /* used to signal the kernel menu thread when there are no processes */
 struct semaphore *no_proc_sem;   
 
-// #define PID_SIZE (__PID_MAX - __PID_MIN + 1);
-// static proc_state *proc_states[PID_SIZE];
+#define PID_SIZE 32766;
+static proc_state *proc_states[PID_SIZE];
 #endif  // UW
 
 // GLOBAL PROCARRAY
