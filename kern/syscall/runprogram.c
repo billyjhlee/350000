8f920b954392201bd817f0b4aa11a6a23299620e
Junghoon Lee <iamfaddict@gmail.com>
2019-03-08 11:31:04 -0500

    affffff

--- kern/syscall/runprogram.c
+++ kern/syscall/runprogram.c
@@ -89,7 +89,7 @@ runprogram(char *progname, char **args,  unsigned long args_len)
 
 	/* Done with the file now. */
 	vfs_close(v);
-
+	kprintf("runprogram 1\n");
 	/* Define the user stack in the address space */
 	result = as_define_stack(as, &stackptr, args, args_len);
 	if (result) {
@@ -97,9 +97,13 @@ runprogram(char *progname, char **args,  unsigned long args_len)
 		return result;
 	}
 
+	kprintf("runprogram 2\n");
+
 	/* Warp to user mode. */
 	enter_new_process(args_len /*argc*/, (userptr_t) stackptr /*userspace addr of argv*/,
 			  stackptr, entrypoint);
+
+	kprintf("runprogram 3\n");
 	
 	/* enter_new_process does not return. */
 	panic("enter_new_process returned\n");
