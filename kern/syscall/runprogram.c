4813c90f92b83336bcec269b51064f0e949d9330
Junghoon Lee <iamfaddict@gmail.com>
2019-03-08 11:26:10 -0500

    affffff

--- kern/syscall/runprogram.c
+++ kern/syscall/runprogram.c
@@ -52,7 +52,7 @@
  * Calls vfs_open on progname and thus may destroy it.
  */
 int
-runprogram(char *progname)
+runprogram(char *progname, char **args,  unsigned long args_len)
 {
 	struct addrspace *as;
 	struct vnode *v;
@@ -91,14 +91,14 @@ runprogram(char *progname)
 	vfs_close(v);
 
 	/* Define the user stack in the address space */
-	result = as_define_stack(as, &stackptr,0 ,0);
+	result = as_define_stack(as, &stackptr, args, args_len);
 	if (result) {
 		/* p_addrspace will go away when curproc is destroyed */
 		return result;
 	}
 
 	/* Warp to user mode. */
-	enter_new_process(0 /*argc*/, NULL /*userspace addr of argv*/,
+	enter_new_process(argc /*argc*/, (userptr_t) stackptr /*userspace addr of argv*/,
 			  stackptr, entrypoint);
 	
 	/* enter_new_process does not return. */
